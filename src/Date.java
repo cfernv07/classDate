614a31f64e768e0f4b3348cd1176d49ad5eeb24e
Carlos Fernández <cfernv07@estudiantes.unileon.es>
2016-04-05 12:42:18 +0200

    Empezados métodos switch

--- src/Date.java
+++ src/Date.java
@@ -115,4 +115,38 @@ public class Date{
 
 	}
 
+	String getMonthName(){
+
+		String mes="";
+
+		switch(this.month){
+
+			case 1: mes = "Enero";
+			break;
+
+			case 2: mes = "Febrero";
+			break;
+
+			case 3: mes = "Marzo";
+			break;
+
+			case 4: mes = "Abril";
+			break;
+
+			case 5: mes = "Mayo";
+			break;
+
+			case 6: mes = "Junio";
+			break;
+
+			case 7: mes = "Julio";
+			break;
+
+			case 8:
+			
+
+		}
+	
+	}
+
 }
