34772bb6f22ed932c722e87ff9c3fa6a8b11b88e
Carlos Fernández <cfernv07@estudiantes.unileon.es>
2016-04-05 12:25:29 +0200

    Métodos realizados

---
+++ src/Date.java
@@ -0,0 +1,118 @@
+public class Date{
+
+	private int day;
+	private int month;
+	private int year;
+
+	public Date(int day, int month, int year){
+
+		this.day = day;
+		this.month = month;
+		this.year = year;
+
+	}
+
+
+	int getYear(){
+
+		return this.year;
+
+	}
+
+
+	int getMonth(){
+
+		return this.month;
+
+	}
+
+	int getDay(){
+
+		return this.day;
+
+	}
+
+	void setYear(int year){
+
+		this.year = year;
+
+	}
+
+	void setMonth(int month){
+
+		this.month = month;
+
+	}
+
+	void setDay(int day){
+
+		this.day = day;
+
+	}
+
+	boolean isSameYear(Date other){
+
+		if(this.year==other.getYear()){
+
+			return true;
+
+		}
+
+		else{
+
+			return false;
+
+		}
+
+
+	}
+
+	boolean isSameMonth(Date other){
+
+		if(this.month==other.getMonth()){
+
+			return true;
+
+		}
+
+		else{
+
+			return false;
+			
+		}
+
+	}
+
+	boolean isSameDay(Date other){
+
+		if(this.day==other.day){
+
+			return true;
+
+		}
+
+		else{
+
+			return false;
+
+		}
+
+	}
+
+	boolean isSame(Date day, Date month, Date year){
+
+		if((this.day==day.getDay())&&(this.month==month)&&(this.year==year)){
+
+			return true;
+
+		}
+
+		else{
+
+			 return false;
+
+		}
+
+	}
+
+}
