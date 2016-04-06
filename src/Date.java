db2476b1e72f5fc1a208a770bb3096b2f632dcd2
Carlos Fernández <cfernv07@estudiantes.unileon.es>
2016-04-07 01:55:24 +0200

    Queda un ejercicio y las excepciones

--- src/Date.java
+++ src/Date.java
@@ -1,8 +1,18 @@
+ package es.unileon.prg.date;
+
 public class Date{
 
 	private int day;
 	private int month;
 	private int year;
+
+	public Date(){
+
+		this.day = 1;
+		this.month = 1;
+		this.year = 2016;
+		
+	}
 
 	public Date(int day, int month, int year){
 
@@ -12,45 +22,53 @@ public class Date{
 
 	}
 
+	public Date(Date aux){
 
-	int getYear(){
+		this.day = aux.getDay();
+		this.month = aux.getMonth();
+		this.year = aux.getYear();
+
+	}
+
+
+	public int getYear(){
 
 		return this.year;
 
 	}
 
 
-	int getMonth(){
+	public int getMonth(){
 
 		return this.month;
 
 	}
 
-	int getDay(){
+	public int getDay(){
 
 		return this.day;
 
 	}
 
-	void setYear(int year){
+	public void setYear(int year){
 
 		this.year = year;
 
 	}
 
-	void setMonth(int month){
+	public void setMonth(int month){
 
 		this.month = month;
 
 	}
 
-	void setDay(int day){
+	public void setDay(int day){
 
 		this.day = day;
 
 	}
 
-	boolean isSameYear(Date other){
+	public boolean isSameYear(Date other){
 
 		if(this.year==other.getYear()){
 
@@ -67,7 +85,7 @@ public class Date{
 
 	}
 
-	boolean isSameMonth(Date other){
+	public boolean isSameMonth(Date other){
 
 		if(this.month==other.getMonth()){
 
@@ -83,7 +101,7 @@ public class Date{
 
 	}
 
-	boolean isSameDay(Date other){
+	public boolean isSameDay(Date other){
 
 		if(this.day==other.day){
 
@@ -99,9 +117,9 @@ public class Date{
 
 	}
 
-	boolean isSame(Date day, Date month, Date year){
+	public boolean isSame(Date day, Date month, Date year){
 
-		if((this.day==day.getDay())&&(this.month==month)&&(this.year==year)){
+		if((this.day==day.getDay())&&(this.month==month.getMonth())&&(this.year==year.getYear())){
 
 			return true;
 
@@ -115,7 +133,7 @@ public class Date{
 
 	}
 
-	String getMonthName(){
+	public String getMonthName(){
 
 		String mes="";
 
@@ -142,11 +160,341 @@ public class Date{
 			case 7: mes = "Julio";
 			break;
 
-			case 8:
+			case 8: mes = "Agosto";
+			break;
+
+			case 9: mes = "Septiembre";
+			break;
+
+			case 10: mes = "Octubre";
+			break;
+
+			case 11: mes = "Noviembre";
+			break;
+
+			case 12: mes = "Diciembre";
+			break;
 			
 
 		}
+		return mes;
 	
 	}
 
+	public boolean isValidDay(){
+
+		boolean validez=false;
+
+		switch(this.month){
+
+			case 1:
+			case 3:
+			case 5:
+			case 7:
+			case 8:
+			case 10:
+			case 12:
+
+				if((this.day<1)||(this.day>31)){
+			
+					validez = false;			
+
+				}
+
+			break;
+
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+
+				if((this.day<1)||(this.day>30)){
+
+					validez = false;
+
+				}
+
+			break;
+
+			case 2:
+
+				if((this.day<1)||(this.day>28)){
+
+					validez = false;
+				
+				}
+
+			break;
+
+		}
+
+		return validez;
+
+	}
+
+	public String getSeason(){
+
+		String season="";		/*
+							Hemisferio norte
+							Primavera: 21 marzo hasta 20 junio
+							Verano: 21 junio hasta 20 septiembre
+							Otoño: 21 septiembre hasta 20 diciembre
+							Invierno: 21 diciembre hasta 20 marzo
+						*/
+
+		switch(this.month){
+
+			case 1: season = "Invierno";
+			break;
+			case 2: season = "Invierno";
+			break;
+			case 3:
+			
+				if(this.day<21){
+			
+					season = "Invierno";
+
+				}
+				
+				else{
+
+					season = "Primavera";
+
+				}
+
+			break;
+
+			case 4: season = "Primavera";
+			break;
+			case 5: season = "Primavera";
+			break;
+			case 6: 
+
+				if(this.day<21){
+
+					season = "Primavera";
+
+				}
+			
+				else{
+
+					season = "Verano";
+
+				}
+
+			break;
+
+			case 7: season = "Verano";
+			break;
+			case 8: season = "Verano";
+			break;
+			case 9: 
+
+				if(this.day<21){
+
+					season = "Verano";
+
+				}
+
+				else{
+
+					season = "Otoño";
+
+				}
+
+			break;
+
+			case 10: season = "Otoño";
+			break;
+			case 11: season= "Otoño";
+			break;
+			case 12:
+
+				if(this.day<21){
+
+					season ="Otoño";
+
+				}
+
+				else{
+				
+					season = "Invierno";
+	
+				}
+		
+			break;
+			
+
+		}
+
+
+		return season;
+
+	}
+
+	String monthsLeft(){
+
+		StringBuffer months = new StringBuffer();
+
+		Date fecha = new Date(this);
+
+		for(int i=this.month;i<=12;i++){
+
+			fecha.setMonth(fecha.getMonth()+1);
+
+			months.append(" "+fecha.getMonthName());	
+
+		}
+
+		return months.toString();
+
+	}
+
+	String getDate(){
+
+		String date="";
+
+		date = ("La fecha es: "+this.day+"/"+this.month+"/"+this.year);
+
+		return date.toString();
+
+	}
+
+	int getNumDays(){
+
+		int days = 0;
+
+		if((this.month==1)||(this.month==3)||(this.month==5)||(this.month==7)||(this.month==8)||(this.month==10)||(this.month==12)){
+
+			days = 31;
+
+		}
+
+		else if((this.month==4)||(this.month==6)||(this.month==9)||(this.month==11)){
+
+			days = 30;
+
+		}
+
+		else{
+
+			days = 28;
+
+		}
+
+		return days;
+
+	}
+
+	String getDatesUntilEnd(){
+
+		StringBuffer dates = new StringBuffer();
+
+		Date fecha =new Date(this);
+
+		for(int i=this.day;i<=getNumDays();i++){
+
+			fecha.setDay(fecha.getDay()+1);
+
+			dates.append(" "+this.day+"/"+this.month+"/"+this.year);
+
+		}
+
+		return dates.toString();
+
+	}
+
+	String getEqualsMonths(){
+
+		StringBuffer equalMonths = new StringBuffer();
+		
+		int dias = getNumDays();
+
+		int otrosDias = 0;
+
+		Date fecha = new Date(this);
+
+		Date fecha2 = new Date(this);
+
+		for(int i=1;i<=12;i++){
+
+			fecha2.setMonth(i);
+
+			if(dias==fecha2.getNumDays()){
+
+				equalMonths.append(" "+fecha2.getMonthName());
+
+			}	
+		
+		}
+
+		
+
+		return equalMonths.toString();
+
+	}
+
+	int dayCount(){
+
+		int dias = 0;
+
+		Date fecha = new Date(this);
+
+		Date fecha2 = new Date(this);
+
+		for(int i=1;i==fecha2.getMonth();i++){
+
+			fecha.setMonth(i);
+	
+			if(fecha.getMonth()!=i){
+
+				dias = dias + fecha.getNumDays();
+			
+			}
+
+			else{
+
+				dias = dias + fecha2.getDay();
+
+			}
+
+		}
+
+		return dias;
+
+	}
+
+	int getAttemps(){
+
+		int intentos = 0;
+		int randomDay;;
+		int randomMonth;
+		int randomYear;
+
+		do{
+
+			randomDay = (int) (Math.random()*31+1);
+			randomMonth = (int) (Math.random()*12+1);
+			randomYear = (int) (Math.random()*3000);
+
+			intentos++;
+
+		}while(this.year!=randomYear);
+
+		return intentos;
+
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
